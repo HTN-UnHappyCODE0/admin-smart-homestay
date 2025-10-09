e5887fa7081e426468e2f916776722f446f8f1d7
giangndh <gianghoang150503@gmail.com>
2025-10-09 11:39:32 +0700

    update

--- src/components/pages/apartment/MainPageApartment/interfaces/index.ts
+++ src/components/pages/apartment/MainPageApartment/interfaces/index.ts
@@ -1,1 +1,53 @@
 export interface PropsMainPageApartment {}
+
+export interface IApartment {
+	managerUu: {
+		uuid: string;
+		code: string;
+		name: string;
+	};
+	ownerUu: null;
+	lock: {
+		uuid: string;
+		code: string;
+		name: string;
+	};
+	numVisitRequest: number;
+	numIncidentRequest: number;
+	maxPeople: number;
+	rentPrice: number;
+	adPrice: number;
+	apartmentMeters: {
+		serialNumber: string;
+		type: number;
+		value: number;
+		onState: number;
+		id: number;
+		uuid: string;
+		status: number;
+	}[];
+	description: string;
+	attachments: string[];
+	apartmentTypeUu: {
+		uuid: string;
+		code: string;
+		name: string;
+	};
+	name: string;
+	province: {
+		code: string;
+		fullName: string;
+		fullNameEn: string;
+	};
+	ward: {
+		code: string;
+		fullName: string;
+		fullNameEn: string;
+		provinceCode: string;
+	};
+	address: string;
+	apartmentSize: number;
+	id: number;
+	uuid: string;
+	status: number;
+}
---
+++ src/services/apartmentServices.ts
@@ -0,0 +1,30 @@
+import axiosClient from '.';
+
+const apartmentServices = {
+	getListApartments: (
+		data: {
+			keyword: string;
+			isPaging: number;
+			pageSize: number;
+			page: number;
+			typeFinding: number | null;
+			status: number | null;
+			sizeFrom: number | null;
+			sizeTo: number | null;
+			province: string;
+			ward: string;
+		},
+		tokenAxios?: any
+	) => {
+		return axiosClient.post(`/Apartment/get-list-apartments`, data, {
+			cancelToken: tokenAxios,
+		});
+	},
+	changeStatus: (data: {uuid: string; status: number; description: string}, tokenAxios?: any) => {
+		return axiosClient.post(`/Apartment/change-status`, data, {
+			cancelToken: tokenAxios,
+		});
+	},
+};
+
+export default apartmentServices;
