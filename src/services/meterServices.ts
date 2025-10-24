a40546f92c3565c536467ce1f361635b9ee4e4dd
giangndh <gianghoang150503@gmail.com>
2025-10-24 09:28:37 +0700

    updated

--- src/services/meterServices.ts
+++ src/services/meterServices.ts
@@ -20,6 +20,18 @@ const meterServices = {
 			cancelToken: tokenAxios,
 		});
 	},
+	createMeter: (
+		data: {
+			meterType: string;
+			serialNumber: string;
+			apartmentUuid: string;
+			userInstallUuid: string;
+			installedDate: string;
+		},
+		tokenAxios?: any
+	) => {
+		return axiosClient.post(`/Meter/create-meter`, data, {});
+	},
 	listMeterInApartment: (
 		data: {
 			keyword: string;
@@ -48,20 +60,6 @@ const meterServices = {
 		tokenAxios?: any
 	) => {
 		return axiosClient.post(`/Meter/update-meters-in-apartment`, data, {
-			cancelToken: tokenAxios,
-		});
-	},
-	createMeter: (
-		data: {
-			meterType: string;
-			serialNumber: string;
-			apartmentUuid: string;
-			userInstallUuid: string;
-			installedDate: string;
-		},
-		tokenAxios?: any
-	) => {
-		return axiosClient.post(`/Meter/create-meter`, data, {
 			cancelToken: tokenAxios,
 		});
 	},
